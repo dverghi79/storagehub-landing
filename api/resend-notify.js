7287e4873b60a379c8fe7c6099eeb0ac9ac1d64a
Dario Verghi <dario.verghi@gmail.com>
2026-05-07 07:11:43 +0200

    feat: add product property to Resend contact payload

--- api/resend-notify.js
+++ api/resend-notify.js
@@ -42,6 +42,7 @@ module.exports = async function handler(req, res) {
     }
     contactPayload.properties = {
       source: productName,
+      product: productName,
       signed_up_at: new Date().toISOString(),
     };
 
